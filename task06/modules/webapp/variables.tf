171c1ed21b4a3040054443d3107d6d79b86a4f92
priyanshu_kumar <priyanshu_kumar@epam.com>
2025-05-19 22:06:52 +0530

    second

---
+++ task06/modules/webapp/variables.tf
@@ -0,0 +1,40 @@
+variable "resource_group_name" {
+  type = string
+  description = "rg name"
+}
+
+variable "location" {
+  type = string
+  description = "location name"
+}
+
+variable "app_service_plan_name" {
+  type = string
+  description = "asp name"
+}
+
+variable "app_service_plan_sku" {
+  type = string
+  description = "asp sku content"
+}
+
+variable "webapp_name" {
+  type = string
+  description = "webapp name"
+}
+
+variable "dotnet_version" {
+  type = string
+  description = "version of .net"
+}
+
+variable "sql_connection_string" {
+  type      = string
+  sensitive = true
+  description = "connect string"
+}
+
+variable "tags" {
+  type = map(string)
+  description = "tag name"
+}
