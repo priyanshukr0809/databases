e3c70eff6d4069c9d1b82098d2df87470def5d1e
priyanshu_kumar <priyanshu_kumar@epam.com>
2025-05-19 22:32:26 +0530

    changes made

---
+++ task06/outputs.tf
@@ -0,0 +1,9 @@
+output "sql_server_fqdn" {
+  value = module.sql.sql_server_fqdn
+  description = "server fqdn"
+}
+
+output "app_hostname" {
+  value = module.webapp.app_hostname
+  description = "app hostname"
+}
