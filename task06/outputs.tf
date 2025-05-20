a09c56f96f6e9cbf1654ebeee572053df55513d5
PRIYANSHU KUMAR <priyanshu_kumar@epam.com>
2025-05-20 19:23:02 +0530

    Delete task06/outputs.tf

--- task06/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sql_server_fqdn" {
-  value       = module.sql.sql_server_fqdn
-  description = "server fqdn"
-}
-
-output "app_hostname" {
-  value       = module.webapp.app_hostname
-  description = "app hostname"
-}
